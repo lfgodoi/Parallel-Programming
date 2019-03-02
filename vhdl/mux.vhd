3f12eec3dbab1efa8b01812bcdf2f1fb51759167
Leonardo Franco de Godói <eng.leonardogodoi@gmail.com>
2019-03-01 23:58:51 -0300

    Update mux.vhd

--- vhdl/mux.vhd
+++ vhdl/mux.vhd
@@ -13,29 +13,29 @@
 -- Created by leonardo Franco de Godói
 --
 
-    -- Importing libraries
-    library IEEE;
-    use IEE.std_logic_1164.all;
-	
-    -- Setting entity
-    entity mux is port
-    (
+-- Importing libraries
+library IEEE;
+use IEE.std_logic_1164.all;
+
+-- Setting entity
+entity mux is port
+(
 	S : in std_logic_vector(1 downto 0);                          -- Selector array (2 pins)
 	I : in std_logic_vector(3 downto 0);                          -- Input array (4 pins)
 	O : out std_logic                                             -- Output
-    );
-    end mux;
-	
-    -- Setting architecture
-    architecture multiplexing of mux_4_to_1 is
-    begin
-	    
-        -- Setting a value for output depending on selectors state
+);
+end mux;
+
+-- Setting architecture
+architecture multiplexing of mux_4_to_1 is
+begin
+
+	-- Setting a value for output depending on selectors state
 	with S select
-	    O <= I(0) when "00",
-                 I(1) when "01",
-                 I(2) when "10",
-                 I(3) when "11",
-                 '0' when others;			 
-	
-    end multiplexing;
+		O <= I(0) when "00",
+		     I(1) when "01",
+		     I(2) when "10",
+		     I(3) when "11",
+		     '0' when others;			 
+
+end multiplexing;
